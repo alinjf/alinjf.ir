798ca27d93acaf9f8398cd547fbd7a72606caf89
Ali Najafi <najafi.it@gmail.com>
2024-03-17 15:58:30 +0330

    complete crowd platform

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,20 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import remarkToc from "remark-toc";
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        rehypePlugins: [rehypeHeadingIds],
+    },
     site: "https://alinjf.ir",
-    integrations: [sitemap(), tailwind(), mdx()],
+    integrations: [
+        sitemap(),
+        tailwind(),
+        mdx({
+            rehypePlugins: [rehypeHeadingIds],
+            remarkPlugins: [remarkToc],
+        }),
+    ],
 });
